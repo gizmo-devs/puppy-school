6defc6fde36052ce26290ca33af037b06c8e5510
Craig <craig.attwood@hotmail.co.uk>
2020-06-08 16:35:06 +0100

    Comment out inserts / updates for fresh application

--- app/app/data-files/schema.sql
+++ app/app/data-files/schema.sql
@@ -110,45 +110,45 @@ CREATE TABLE dog_walks (
     walked_date TIMESTAMP NULL
 );
 
-
--- Add a user
-INSERT INTO users (first_name, surname, email, password) VALUES
-    ('Craig', 'Attwood', 'admin@admin', 'pbkdf2:sha256:150000$gel9CvHW$ff526d0db176b2a91023afaecd31fa92f00625444f5c5f9a314f04bf5bc43499');
-
--- Add a dog
-INSERT INTO dogs (name, dob, gender, bread) VALUES
-    ('Tiggie', '2020-04-13', 'F', 'CockerSpaniel'),
-    ('Milo', '2020-10-1', 'M', 'CockerPoo');
-
-INSERT INTO dog_last (dog_id, last_loo_break, last_fed, last_weighed) VALUES
-    (1, null, null, null),
-    (2, null, null, null);
-
--- assign dogs to users
-INSERT INTO dog_owners (dog_id, user_id) VALUES
-    (1,1),
-    (2,1);
-
--- add dummy data for weight graphs
-INSERT INTO dog_weights (dog_id, measured_by, weight, date_measured) VALUES
-    (1, 1, 1.6, "2020-04-28"),
-    (1, 1, 2.0, "2020-05-01"),
-    (1, 1, 2.1, "2020-05-13"),
-    (1, 1, 2.7, "2020-05-20"),
-    (1, 1, 3.0, "2020-05-27"),
-    (2, 1, 3.0, "2020-03-27"),
-    (2, 1, 3.0, "2020-03-31");
-
--- add dog commands
-INSERT INTO dog_commands (command, level) VALUES
-    ('Sit', 'Easy'),
-    ('Down', 'Easy'),
-    ('Road Trip', 'Hard');
-
--- Link dogs to commands
-INSERT INTO dog_training_link (dog_id, command_id) VALUES
-    (1,1), (1,2), (1,3);
-
-INSERT INTO dog_training_progress (dog_id, command_id, command_progress, rec_date) VALUES
-    (1, 1, 1, "2020-06-03 12:44:11.598258"), (1, 1, 2, "2020-06-03 12:44:11.598258"), (1, 1, 3, "2020-06-03 12:44:11.598258"), (1, 1, 4,"2020-06-03 12:44:11.598258");
-
+--
+---- Add a user
+--INSERT INTO users (first_name, surname, email, password) VALUES
+--    ('Craig', 'Attwood', 'admin@admin', 'pbkdf2:sha256:150000$gel9CvHW$ff526d0db176b2a91023afaecd31fa92f00625444f5c5f9a314f04bf5bc43499');
+--
+---- Add a dog
+--INSERT INTO dogs (name, dob, gender, bread) VALUES
+--    ('Tiggie', '2020-04-13', 'F', 'CockerSpaniel'),
+--    ('Milo', '2020-10-1', 'M', 'CockerPoo');
+--
+--INSERT INTO dog_last (dog_id, last_loo_break, last_fed, last_weighed) VALUES
+--    (1, null, null, null),
+--    (2, null, null, null);
+--
+---- assign dogs to users
+--INSERT INTO dog_owners (dog_id, user_id) VALUES
+--    (1,1),
+--    (2,1);
+--
+---- add dummy data for weight graphs
+--INSERT INTO dog_weights (dog_id, measured_by, weight, date_measured) VALUES
+--    (1, 1, 1.6, "2020-04-28"),
+--    (1, 1, 2.0, "2020-05-01"),
+--    (1, 1, 2.1, "2020-05-13"),
+--    (1, 1, 2.7, "2020-05-20"),
+--    (1, 1, 3.0, "2020-05-27"),
+--    (2, 1, 3.0, "2020-03-27"),
+--    (2, 1, 3.0, "2020-03-31");
+--
+---- add dog commands
+--INSERT INTO dog_commands (command, level) VALUES
+--    ('Sit', 'Easy'),
+--    ('Down', 'Easy'),
+--    ('Road Trip', 'Hard');
+--
+---- Link dogs to commands
+--INSERT INTO dog_training_link (dog_id, command_id) VALUES
+--    (1,1), (1,2), (1,3);
+--
+--INSERT INTO dog_training_progress (dog_id, command_id, command_progress, rec_date) VALUES
+--    (1, 1, 1, "2020-06-03 12:44:11.598258"), (1, 1, 2, "2020-06-03 12:44:11.598258"), (1, 1, 3, "2020-06-03 12:44:11.598258"), (1, 1, 4,"2020-06-03 12:44:11.598258");
+--
